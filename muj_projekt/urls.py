3300b3cfe83abb9fac8f6c9e9e70b3eda4d65e72
Matej <d23321@oa-opava.cz>
2026-06-03 22:24:55 +0200

    Nasazeni kompletniho noveho HTML

--- muj_projekt/urls.py
+++ muj_projekt/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from django.urls import path
-from skins import views as skin_views
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from skins import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', skin_views.prehled_skinu, name='prehled_skinu'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/register/', skin_views.register, name='register'),
-    
-    path('smazat/<int:skin_id>/', skin_views.smazat_skinu, name='smazat_skinu'),
-    path('status/<int:skin_id>/', skin_views.zmenit_status, name='zmenit_status'),
+    path('', views.prehled_skinu, name='prehled_skinu'),  # Hlavní stránka poběží hned na základní adrese
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', views.register, name='register'),
+    path('smazat/<int:skin_id>/', views.smazat_skinu, name='smazat_skinu'),
+    path('status/<int:skin_id>/', views.zmenit_status, name='zmenit_status'),
 ]
