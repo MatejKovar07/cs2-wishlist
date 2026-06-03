56827d2e7d3f3b5f17f7eab200a0910ea8ab14eb
Matej <d23321@oa-opava.cz>
2026-06-03 22:31:53 +0200

    Uplna oprava formularu, URL adres a chyb v logu

--- muj_projekt/settings.py
+++ muj_projekt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ecv1^^lmk*vr481wtr6suipkjco)3ve53q#yufw9a7-roqz0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['makousek1.eu.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
